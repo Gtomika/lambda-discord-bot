ea804d663f5187ef858445fc93fe6a2cba6ff793
Gáspár Tamás <gaspar.tomi97@gmail.com>
2023-04-03 16:11:09 +0200

    Set route target

--- terraform/api_gateway/main.tf
+++ terraform/api_gateway/main.tf
@@ -15,11 +15,6 @@ resource "aws_apigatewayv2_api" "api_gateway" {
   description = "API Gateway for ${var.app_name} application, forwarding request to the discord interaction lambda"
 }
 
-resource "aws_apigatewayv2_route" "discord_interaction_route" {
-  api_id    = aws_apigatewayv2_api.api_gateway.id
-  route_key = "POST /${var.discord_interaction_path}"
-}
-
 resource "aws_apigatewayv2_integration" "discord_interaction_integration" {
   api_id           = aws_apigatewayv2_api.api_gateway.id
   integration_type = "AWS_PROXY"
@@ -29,14 +24,31 @@ resource "aws_apigatewayv2_integration" "discord_interaction_integration" {
   integration_uri           = var.discord_interaction_lambda_invocation_arn
 }
 
-# API gateway stage and deployment
+resource "aws_apigatewayv2_route" "discord_interaction_route" {
+  api_id    = aws_apigatewayv2_api.api_gateway.id
+  route_key = "POST /${var.discord_interaction_path}"
+  target = "integrations/${aws_apigatewayv2_integration.discord_interaction_integration.id}"
+}
 
+# API gateway stage
 resource "aws_apigatewayv2_stage" "api_gateway_stage" {
   name          = var.environment
   api_id        = aws_apigatewayv2_api.api_gateway.id
   access_log_settings {
     destination_arn = aws_cloudwatch_log_group.api_gateway_log_group.arn
-    format          = "$context.identity.sourceIp $context.identity.caller $context.identity.user [$context.requestTime] $context.httpMethod $context.resourcePath $context.protocol $context.status $context.responseLength $context.requestId $context.extendedRequestId"
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
   }
 }
 
