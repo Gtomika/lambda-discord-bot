1b8dbd03524167c46ee7ce7a8fd5fc5ddf53cba9
Tamas Gaspar <gaspar.tamas97@protonmail.com>
2025-06-28 15:46:37 +0200

    Upgrade several versions.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  backend "s3" {
+    bucket = "tamas-gaspar-epam-cloudx-terraform-state"
+    key = "LambdaDiscordBot.tfstate"
+    region = "eu-central-1"
+  }
+}
+
 provider "aws" {
   # these credentials do not allow access: for that role must be assumed
   access_key = var.aws_key_id
@@ -16,13 +30,5 @@ provider "aws" {
       repository = "https://github.com/Gtomika/lambda-discord-bot"
       owner = "Tamas Gaspar"
     }
-  }
-}
-
-terraform {
-  backend "s3" {
-    bucket = "tamas-gaspar-epam-cloudx-terraform-state"
-    key = "LambdaDiscordBot.tfstate"
-    region = "eu-central-1"
   }
 }
